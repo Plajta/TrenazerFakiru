bb908a5aceea65ac3fa75fae5e3a13a1bc69c456
HelloWorld7894 <danpojhan@gmail.com>
2022-05-06 23:26:30 +0200

    Added proper Exit

--- src/Main.py
+++ src/Main.py
@@ -10,20 +10,24 @@ def Main():
     A.ViewDevices()
     A.CreateStream(8)
     while A.stream.is_active():
+        try:
+            A.Buffer[:-A.FRAME_SIZE] = A.Buffer[A.FRAME_SIZE:]
+            A.Buffer[-A.FRAME_SIZE:] = np.fromstring(A.stream.read(A.FRAME_SIZE), np.int16)
+
+            #Run FFT
+            fft = np.fft.rfft(A.Buffer * A.Hann_Win)
+            freq = (np.abs(fft[A.imin:A.imax]).argmax() + A.imin) * A.FREQ_STEP
+
+            n = A.freq_to_number(freq)
+            n0 = int(round(n))
+
+            num_frames += 1
+            if num_frames >= A.FRAMES_PER_FFT:
+                print("freq: {:7.2f} Hz     note: {:>3s} {:+.2f}".format(freq, A.note_name(n0), n-n0))
+        except KeyboardInterrupt:
+            A.DestroyStream()
+            print("Exited successfully")
         
-        A.Buffer[:-A.FRAME_SIZE] = A.Buffer[A.FRAME_SIZE:]
-        A.Buffer[-A.FRAME_SIZE:] = np.fromstring(A.stream.read(A.FRAME_SIZE), np.int16)
-
-        #Run FFT
-        fft = np.fft.rfft(A.Buffer * A.Hann_Win)
-        freq = (np.abs(fft[A.imin:A.imax]).argmax() + A.imin) * A.FREQ_STEP
-
-        n = A.freq_to_number(freq)
-        n0 = int(round(n))
-
-        num_frames += 1
-        if num_frames >= A.FRAMES_PER_FFT:
-            print("freq: {:7.2f} Hz     note: {:>3s} {:+.2f}".format(freq, A.note_name(n0), n-n0))
 
 
 if __name__ == "__main__":
