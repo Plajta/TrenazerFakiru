04fa02766e5f825f99cb6f7704c780065970f514
HelloWorld7894 <danpojhan@gmail.com>
2022-05-06 21:59:44 +0200

    Commit

--- src/Audio.py
+++ src/Audio.py
@@ -1,2 +1,13 @@
 import numpy as np
 import pyaudio
+
+SAMPLE = 22050
+FRAME_SIZE = 2048
+
+def CreateStream():
+    global stream
+    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
+        channels=1,
+        rate=SAMPLE,
+        input=True,
+        frames_per_buffer=FRAME_SIZE)
