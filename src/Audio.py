c7569792202b60bd5b696a3f0a383f7d25380719
HelloWorld7894 <danpojhan@gmail.com>
2022-05-06 22:34:15 +0200

    Added stream functions

--- src/Audio.py
+++ src/Audio.py
@@ -4,10 +4,26 @@ import pyaudio
 SAMPLE = 22050
 FRAME_SIZE = 2048
 
-def CreateStream():
+#Setup
+PyAudioInstance = pyaudio.PyAudio()
+def ViewDevices():
+    info = PyAudioInstance.get_host_api_info_by_index(0)
+    numdevices = info.get("deviceCount")
+
+    for i in range(numdevices):
+        if (PyAudioInstance.get_device_info_by_host_api_device_index(0, i).get("maxInputChannels")) > 0:
+            print("Input Device id ", i, " - ", PyAudioInstance.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+def CreateStream(InputDeviceIndex):
     global stream
-    stream = pyaudio.PyAudio().open(format=pyaudio.paInt16,
+    stream = PyAudioInstance.open(format=pyaudio.paInt16,
         channels=1,
         rate=SAMPLE,
         input=True,
-        frames_per_buffer=FRAME_SIZE)
+        frames_per_buffer=FRAME_SIZE,
+        input_device_index=InputDeviceIndex
+    )
+    stream.start_stream()
+
+def StopStream(): stream.stop_stream()
+def DestroyStream(): stream.close()
