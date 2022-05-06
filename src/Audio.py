2dc7585399f5c975e4a387c79f00f45dc25e9fcf
Vesek <venda.straka@gmail.com>
2022-05-07 00:21:57 +0200

    Fixed video and merged it with audio

--- src/Audio.py
+++ src/Audio.py
@@ -5,27 +5,10 @@ NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
 SAMPLE = 22050
 FRAME_SIZE = 2048
-FRAMES_PER_FFT = 16
-SAMPLES_PER_FFT = FRAME_SIZE * FRAMES_PER_FFT
-FREQ_STEP = float(SAMPLE) / SAMPLES_PER_FFT
-NOTE_MIN = 60 #C4
-NOTE_MAX = 69 #A4
+
 
 #Setup
 PyAudioInstance = pyaudio.PyAudio()
-Buffer = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
-#Hanning Window Function
-Hann_Win = 0.5 * (1 - np.cos(np.linspace(0, 2 * np.pi, SAMPLES_PER_FFT, False)))
-
-#Conversion
-def freq_to_number(f): return 69 + 12 * np.log2(f / 440.0)
-def number_to_freq(n): return 440 * 2.0 ** ((n - 69) / 12.0)
-def note_name(n): return NOTES[n % 12] + str(n / 12 - 1)
-def note_to_fftbin(n): return number_to_freq(n) / FREQ_STEP
-
-imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN - 1))))
-imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX + 1))))
-
 def ViewDevices():
     info = PyAudioInstance.get_host_api_info_by_index(0)
     numdevices = info.get("deviceCount")
@@ -44,8 +27,6 @@ def CreateStream(InputDeviceIndex):
         input_device_index=InputDeviceIndex
     )
     stream.start_stream()
-
-
 
 def StopStream(): stream.stop_stream()
 def DestroyStream(): stream.close()
