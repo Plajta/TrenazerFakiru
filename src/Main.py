8aaa28274ee4cda963a1a13a5ca51c1b391f4d77
HelloWorld7894 <danpojhan@gmail.com>
2022-05-07 14:06:56 +0200

    Added video control

--- src/Main.py
+++ src/Main.py
@@ -5,10 +5,12 @@ import cv2
 import sounddevice as sd
 import numpy as np
 
-Last_loc = ((2, 2))
+mouse = Controller()
+joystick = np.zeros(2)
 
 def Main():
     global Last_loc
+
     video = V.init(0)
 
     #try:
@@ -25,6 +27,16 @@ def Main():
 
                     V.Wrists[i][0] = np.round(hand_landmarks.landmark[V.mp_hands.HandLandmark.WRIST].x * im_width)
                     V.Wrists[i][1] = np.round(hand_landmarks.landmark[V.mp_hands.HandLandmark.WRIST].y * im_height)
+            else:
+                V.Wrists = [[320, 240], [320, 240]]
+
+            
+            joystick[0] = 320 - (V.Wrists[0][0] + V.Wrists[1][0]) / 2
+            joystick[1] = ((V.Wrists[0][1] + V.Wrists[1][1]) / 2) - 240
+
+            print(joystick)
+
+            mouse.move(joystick[0], joystick[1])
 
             cv2.imshow('frame', image)
             if cv2.waitKey(1) & 0xFF == ord('q'):
--- src/Video.py
+++ src/Video.py
@@ -44,28 +44,5 @@ def HandsDetect(image):
                 mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS, 
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
-        else:
-            Wrists = [[0, 0], [0, 0]]
 
         return cv2.flip(image, 1), results
-
-def WristCalc(hands, im_width, im_height):
-    if hands.multi_hand_landmarks:
-        for i, hand_landmarks in enumerate(hands.multi_hand_landmarks):
-            Wrists[i][0] = np.round(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].x * im_width)
-            Wrists[i][1] = np.round(hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y * im_height)
-
-def DiffCalc(Diff):
-    DiffX, DiffY = 0, 0
-    if (Diff[1][0] != 0 and Diff[1][1] != 0) or (Diff[0][0] != 0 and Diff[0][1] != 0):
-        DiffX = (Diff[0][0] + Diff[1][0]) / 2
-        DiffY = (Diff[0][1] + Diff[1][1]) / 2
-    return DiffX, DiffY
-    
-def AbsolteMouse(abs_pos):
-    x = (abs_pos[0][0]+abs_pos[1][0])/2
-    y = (abs_pos[0][1]+abs_pos[1][1])/2
-    mouse.position = (1920-4*abs_pos[0], 2*abs_pos[1])
-
-def RelativeMouse(DiffX, DiffY):
-    mouse.move(DiffX, -DiffY)
